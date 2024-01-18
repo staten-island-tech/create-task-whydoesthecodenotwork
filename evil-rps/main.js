c3d51d50bc090987b2996b21d7ba861825ccef99
whydoesthecodenotwork <lawrencel54@nycstudents.net>
2024-01-18 13:11:38 -0500

    fighting
    violence is the answer in this

--- evil-rps/main.js
+++ evil-rps/main.js
@@ -1,8 +1,19 @@
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - 1)) + min;
+}
+
 const gameData = {
     health: 100,
     maxHealth: 100,
+    attack: 10,
     damage: 0,
+    move: null,
     hurt: null,
+    encounter: [{ name: "steve", health: 20, choice: "" }],
+};
+
+const dom = {
+    dialog: document.querySelector("dialog"),
 };
 
 function rps(player, enemy) {
@@ -31,43 +42,141 @@ function rps(player, enemy) {
     }
 }
 
+// it's back
+function nuhUh(element) {
+    element.addEventListener("keydown", (event) => {
+        if (event.key === "Escape") {
+            event.preventDefault();
+        }
+    });
+}
+nuhUh(dom.dialog);
+
 function damage(x) {
+    console.log(x);
     gameData.damage += x;
     if (!gameData.hurt) {
         gameData.hurt = setInterval(() => {
             if (gameData.damage > 0) {
-                gameData.damage--;
-                gameData.health--;
+                if (gameData.health < 1) {
+                    gameData.damage = 0;
+                    dead();
+                } else {
+                    gameData.damage--;
+                    gameData.health--;
+                }
                 updateHealth();
             } else {
+                // player has stopped hurting
                 console.log("change the world my final message goodbye");
                 clearInterval(gameData.hurt);
                 gameData.hurt = null;
             }
-        }, 500);
+        }, 30);
     }
 }
 
 function updateHealth() {
     const redpercent = ((gameData.health - gameData.damage) / gameData.maxHealth) * 100;
     const whitepercent = (gameData.health / gameData.maxHealth) * 100;
+    const color = redpercent <= 0 ? (gameData.damage % 2 === 0 ? "red" : "yellow") : "yellow";
     document.querySelector(
         "#healthbar"
-    ).style.backgroundImage = `linear-gradient(to top, red 0 ${redpercent}%, yellow ${redpercent}%, white ${whitepercent}%, black ${whitepercent}% 100%)`;
+    ).style.backgroundImage = `linear-gradient(to top, red 0 ${redpercent}%, ${color} ${redpercent}%, white ${whitepercent}%, black ${whitepercent}% 100%)`;
     document.querySelector("#health").innerHTML = gameData.health;
 }
 
 document.querySelector("#hurt").addEventListener("click", () => {
-    damage(10);
+    move();
+    // damage(getRandomInt(1, 4) * 5);
 });
+
 document.querySelector("#unhurt").addEventListener("click", () => {
     clearInterval(gameData.hurt);
     gameData.hurt = null;
-    console.log(gameData.health, gameData.damage);
     // remove all pending damage, then restore 80% of missing health
     gameData.health += Math.ceil((gameData.maxHealth - (gameData.health - gameData.damage)) * 0.8) - gameData.damage;
     gameData.damage = 0;
     updateHealth();
 });
+
+function dead() {
+    console.log("you are dead. oho no");
+}
+
+function move() {
+    document.querySelector("#stage progress").value += 0.1;
+    if (getRandomInt(1, 10) === 1) {
+        combat();
+    }
+}
+
+function combat() {
+    console.log("a");
+    dom.dialog.innerHTML = `
+    <h1>holy guacamole</h1>
+    <div id="enemies"></div>
+    <div id="attacks">
+        <button>r</button>
+        <button>p</button>
+        <button>s</button>
+    </div>
+    `;
+    updateEnemies();
+    dom.dialog.appendChild(document.querySelector("#healthbar"));
+    Array.from(document.querySelectorAll("dialog #attacks button")).forEach((button) => {
+        button.addEventListener("click", () => {
+            console.log(`ujhhh this is ${button.innerHTML}`);
+            attack(button.innerHTML);
+        });
+    });
+    dom.dialog.showModal();
+}
+
+function attack(choice) {
+    gameData.encounter.forEach((enemy) => {
+        switch (rps(choice, enemy.choice)) {
+            case "win":
+                console.log("you winner");
+                enemy.health -= gameData.attack;
+                break;
+            case "lose":
+                console.log("you losted");
+                damage(20);
+                break;
+            case "tie":
+                console.log("holy hell");
+                break;
+        }
+    });
+    console.log(gameData.encounter);
+    console.log(
+        gameData.encounter.filter((enemy) => {
+            return enemy.health > 0;
+        })
+    );
+    if (
+        gameData.encounter.filter((enemy) => {
+            return enemy.health > 0;
+        }).length === 0
+    ) {
+        joever();
+    } else {
+        updateEnemies();
+    }
+}
+
+function updateEnemies() {
+    dom.dialog.querySelector("#enemies").replaceChildren();
+    gameData.encounter.forEach((enemy) => {
+        enemy.choice = "rps"[getRandomInt(0, 2)];
+        dom.dialog.querySelector("#enemies").insertAdjacentHTML("beforeend", `<p>this is ${enemy.name} with ${enemy.health}hp and choiced ${enemy.choice}</p>`);
+    });
+}
+
+function joever() {
+    document.querySelector("#stage").after(dom.dialog.querySelector("#healthbar"));
+    dom.dialog.close();
+}
 
 updateHealth();
